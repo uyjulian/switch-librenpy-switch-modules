657d1d46d430fdc37ecab599f0f236eca512ce17
uyjulian <uyjulian@gmail.com>
2021-05-02 10:19:18 -0500

    Use mirror for devkitpro-pkgbuild-helpers instead of devkitPro host

--- setup-docker.bash
+++ setup-docker.bash
@@ -1,6 +1,7 @@
 
 docker run -d --name switchdev --workdir /build/git -v "${PWD}:/build/git" devkitpro/devkita64:20210306 tail -f /dev/null
-docker exec switchdev /bin/bash -c 'dkp-pacman -Sy --noconfirm devkitpro-pkgbuild-helpers'
+curl -LOC - https://github.com/uyjulian/pacman-packages/releases/download/v2.2.3-1-pkgbuild-helpers/devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz
+docker exec switchdev /bin/bash -c 'dkp-pacman -U --noconfirm devkitpro-pkgbuild-helpers-2.2.3-1-any.pkg.tar.xz'
 docker exec switchdev /bin/bash -c 'apt-get -y update'
 docker exec switchdev /bin/bash -c 'apt-get -y install python python-pip libc6-dev'
 docker exec switchdev /bin/bash -c 'python2 -m pip install cython==0.29.21'
